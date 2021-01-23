98789b218f1d4b6a3553f50fbabaf50d13554b15
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 00:56:46 +0100

    Created Email component

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Name from "./Name";
+import Email from "./Email";
 
 function Form(){
   return(
     <div className="Form">
       <Name />
+      <Email />
     </div>
   );
 }
