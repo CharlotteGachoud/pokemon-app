318d1ed3b1bc62ed40693d316d1a159d3900740c
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 00:45:43 +0100

    Created Form component

--- src/SurveyForm.js
+++ src/SurveyForm.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Form from "./Form";
 
 function SurveyForm(){
   return(
     <div className="SurveyForm">
       <Header />
+      <Form />
     </div>
   );
 }
