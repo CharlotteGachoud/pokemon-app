77ab062f44f612991f768a396e5fa0a2566f8f95
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 00:51:05 +0100

    Created Name component

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Name from "./Name";
 
 function Form(){
   return(
     <div className="Form">
-      
+      <Name />
     </div>
-  )
+  );
 }
 
 export default Form;
---
+++ src/Name.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Name(){
+  return(
+    <div className="Name">
+      <div className="form-group">
+        <div className="spacing">
+          <label for="name" id="name-label" className="labels">
+            Name
+          </label>
+        </div>
+        <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name" required />
+      </div>
+    </div>
+  );
+}
+
+export default Name;
