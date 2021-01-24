e6c32f6cc6f4d0561caaefe7e7545c441fed8e74
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 01:48:25 +0100

    Created Footer component

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <div className="Footer">
+     <div className="code-by">
+        <a href="https://github.com/CharlotteGachoud/pokemon-app" className="github-link" target="_blank" rel="noreferrer">
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a href="https://www.linkedin.com/in/charlotte-gachoud/" className="linkedIn-link" target="_blank" rel="noreferrer">
+            Charlotte Gachoud
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/SurveyForm.js
+++ src/SurveyForm.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Form from "./Form";
+import Footer from "./Footer";
 
 function SurveyForm(){
   return(
     <div className="SurveyForm">
       <Header />
       <Form />
+      <Footer />
     </div>
   );
 }
