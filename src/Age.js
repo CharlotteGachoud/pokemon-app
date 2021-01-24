1e2701722632187117d56cd12fd066737805522d
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 01:02:38 +0100

    Created Age component

---
+++ src/Age.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Age() {
+  return(
+    <div className="Age">
+      <div className="spacing">
+        <label for="number" id="number-label" className="labels">
+          How old were you when you first started playing Pokemon?
+        </label>
+      </div>
+      <input type="number" className="form-control" id="number" name="number" placeholder="Age" min="5" max="99" /> 
+    </div>
+  );
+}
+
+export default Age;
--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Name from "./Name";
 import Email from "./Email";
+import Age from "./Age";
 
 function Form(){
   return(
     <div className="Form">
       <Name />
       <Email />
+      <Age />
     </div>
   );
 }
