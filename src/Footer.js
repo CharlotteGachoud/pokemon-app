e686f1952e63407135e38a652b641e024362c3e6
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-24 23:08:26 +0100

    Changed colors of link of Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,14 +4,16 @@ import "./Footer.css";
 function Footer(){
   return(
     <div className="Footer">
-     <div className="code-by">
-        <a href="https://github.com/CharlotteGachoud/pokemon-app" className="github-link" target="_blank" rel="noreferrer">
-          Open-source code
-        </a>{" "}
-        by{" "}
-        <a href="https://www.linkedin.com/in/charlotte-gachoud/" className="linkedIn-link" target="_blank" rel="noreferrer">
-            Charlotte Gachoud
-        </a>
+      <div className="wrap">
+        <div className="code-by">
+          <a href="https://github.com/CharlotteGachoud/pokemon-app" className="github-link" target="_blank" rel="noreferrer">
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a href="https://www.linkedin.com/in/charlotte-gachoud/" className="linkedIn-link" target="_blank" rel="noreferrer">
+              Charlotte Gachoud
+          </a>
+        </div>
       </div>
     </div>
   );
